4a60f7e9b3959a4535dc6bad7513c70422054e6c
EvAnGEL0 <89699138+EvAnGEL0@users.noreply.github.com>
2024-12-18 09:21:12 +0000

    css header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom"
 import styles from "./Header.module.css"
 export const Header =()=>{
     return <div className={styles.container}>
-            <div>
+            <div >
                 <h1>ARMAGGEDON V</h1>
                 <div><p>Сервис мониторнинга и уничтожения астеройдов, опасно приближающихся к Земле.</p></div>
             </div>
-            <div>
-                <Link to={"/asteroids"}>Астероиды</Link>
-                <Link to={"/desroyment"}>Уничтожение</Link>
+            <div className={styles.Link}>
+                <p>
+                <Link to={"/asteroids"}>Астероиды</Link> / <Link to={"/desroyment"}>Уничтожение</Link>
+                </p>
             </div>
     </div>
 }
